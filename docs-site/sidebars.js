939b4ffd43823bcffe07352f4b56c3e1c0ae7d79
rameezqadir <rameezfudda@gmail.com>
2025-12-09 21:54:26 +0500

    Added Docusaurus framework

--- docs-site/sidebars.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  docs: [
-    {
-      type: 'category',
-      label: 'Course',
-      items: ['constitution', 'module-1-ros2', 'module-2-digital-twin', 'module-3-isaac', 'module-4-vla'],
-    },
-  ],
-};
